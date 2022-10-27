2aaa2f4a2a1dc1c68e88fe5f73afa18958107da1
chris-mafra <mafradesouza.cs@gmail.com>
2022-10-27 04:35:38 -0300

    remove vue leaflet

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import Map from '../components/Map.vue';
-import DataContainer from '../components/DataContainer.vue';
 </script>
 
 <template>
   <div class="grid grid-cols-3">
-    <div class="col-span-2"></div>
+    <div class="col-span-2">
+      <Map></Map>
+    </div>
     <DataContainer></DataContainer>
   </div>
 </template>
