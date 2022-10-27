01d04cf9bf7e8b6e0d645e0f92dfde87dab69310
chris-mafra <mafradesouza.cs@gmail.com>
2022-10-27 05:54:01 -0300

    map

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,3 +1,20 @@
-<template></template>
+<template>
+  <div id="map" class="h-96"></div>
+</template>
 
-<script></script>
+<script setup lang="ts">
+import leaflet from 'leaflet';
+
+let mymap;
+onMounted(() => {
+  mymap = leaflet.map('map').setView([51.505, -0.09], 13);
+
+  leaflet
+    .tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    })
+    .addTo(mymap);
+});
+</script>
